6ab0a4e8ea485ee8b29e19e4de5000c3a7f18fe0
Martijn Vegter <martijn@martijnvegter.com>
2019-11-25 12:02:38 +0100

    Different test structure

--- server/test/index.test.ts
+++ server/test/index.test.ts
@@ -1,50 +1,12 @@
 import app from '../src';
-import { expect } from 'chai';
-import request from 'supertest';
-import jsYaml from 'js-yaml';
-import fs from 'fs';
-import appRoot from 'app-root-path';
-import { OpenApiValidator } from 'express-openapi-validate';
 
 /*
- * Load the OpenAPI document.
- */
-const SPEC_FILE = `${appRoot}/spec/openapi.yaml`;
-const openApiDocument = jsYaml.safeLoad(fs.readFileSync(SPEC_FILE, 'utf-8'));
-
-/*
- * Create the validator from the spec document.
- */
-const validator = new OpenApiValidator(openApiDocument, {});
-
-/*
- * HACK: makes sure that the app is ready before starting the tests.
+ * Makes sure that the app is ready, and thus has a database connection, before starting any test(s).
  */
 before((done) => app.on('listening', done));
 
-describe('Post Endpoints', () => {
-    it('should create a new post', async () => {
-        return request(app)
-            .get('/')
-            .expect(200)
-            .then((res) => {
-                expect(res.status).to.equal(200);
-            })
-            .catch((err) => expect(err).to.be.undefined);
-    });
-});
+import specTest from './spec';
 
-describe('index', () => {
-    // Create the response validator for the GET / endpoint
-    const validateResponse = validator.validateResponse('get', '/users');
-
-    it('should return a valid response', () => {
-        return request(app)
-            .get('/v1/users')
-            .expect(200)
-            .then((res) => {
-                expect(validateResponse(res)).to.be.undefined; //validate
-            })
-            .catch((err) => expect(err).to.be.undefined);
-    });
+describe('', () => {
+    describe('Spec', specTest);
 });

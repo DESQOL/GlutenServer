10c1069d8ac8a58da05be5b936c45d163603b97c
Martijn Vegter <martijn@martijnvegter.com>
2019-12-09 11:48:57 +0100

    Updated /v1/users spec tests to include token testing

--- server/test/spec/v1/users.spec.test.ts
+++ server/test/spec/v1/users.spec.test.ts
@@ -27,8 +27,26 @@ export default () => {
         it('should return a valid response', () => {
             return request(app)
                 .get('/v1/users')
+                .set({ 'X-API-KEY': 'sometoken' })
                 .expect(200)
                 .then((res) => expect(responseValidator(res)).to.be.undefined)
+                .catch((err) => expect(err).to.be.undefined);
+        });
+
+        it('should return `401 Unauthorized` when no token is provided', () => {
+            return request(app)
+                .get('/v1/users')
+                .expect(401)
+                .then((res) => {
+                    /* Validate spec */
+                    expect(responseValidator(res)).to.be.undefined;
+
+                    /* Validate response content */
+                    expect(res.body.message).to.equal("'X-API-KEY' header required");
+                    expect(res.body.errors.length).to.equal(1);
+                    expect(res.body.errors[0].path).to.equal('/v1/users');
+                    expect(res.body.errors[0].message).to.equal("'X-API-KEY' header required");
+                })
                 .catch((err) => expect(err).to.be.undefined);
         });
     });

e168336cd53fd2e75da881eb1155bc02600ab524
Martijn Vegter <martijn@martijnvegter.com>
2019-12-13 10:57:00 +0100

    feat: added minimal openapi spec with validator integration

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,21 +1,36 @@
-import * as express from 'express';
+import appRoot from 'app-root-path';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import { OpenApiValidator } from 'express-openapi-validator';
 
 class App {
-  public app: express.Application;
+  public app: Application;
   public port: number;
 
   constructor (port: number = 3000) {
     this.port = port;
     this.app = express();
 
+    new OpenApiValidator({
+      apiSpec: `${appRoot}/spec/openapi.yaml`,
+      validateRequests: true,
+      validateResponses: true,
+    }).installSync(this.app);
+
     this.middlewares();
     this.routes();
+
+    this.app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+      res.status(err.status || 500).json({
+        message: err.message,
+        errors: err.errors,
+      });
+    });
   }
 
   public listen () {
     this.app.listen(this.port, () => {
-            // tslint:disable-next-line
-            console.log(`App listening on the http://localhost:${this.port}`);
+        // tslint:disable-next-line:no-console
+      console.log(`App listening on the http://localhost:${this.port}`);
     });
   }
 

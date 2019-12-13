5961b2e5e7299353d556418b0f85ab31c435b1ed
Martijn Vegter <martijn@martijnvegter.com>
2019-12-13 15:07:32 +0100

    feat: require api-key in either header or query

--- server/test/spec/users.spec.test.ts
+++ server/test/spec/users.spec.test.ts
@@ -20,23 +20,46 @@ export default () => {
 
   describe('GET /users', () => {
     const endpoint: string = '/v1/users';
+    const responses: object = {
+      200: [
+        {
+          id: 1,
+          name: 'Martijn',
+        },
+      ],
+      401: {
+        message: "'X-API-KEY' header required",
+        errors: [
+          {
+            path: '/v1/users',
+            message: "'X-API-KEY' header required",
+          },
+        ],
+      },
+    };
 
-    it('should return a valid response', async () => {
+    it('should return 401 if the api key is missing', async () => {
       request(app)
         .get(endpoint)
+        .expect(401)
+        .then((res) => expect(res.body).to.deep.equal(responses['401']))
+        .catch((err) => expect(err).to.be.undefined);
+    });
+
+    it('should return 200 if header \'X-API-KEY\' is provided', async () => {
+      request(app)
+        .get(endpoint)
+        .set('X-API-KEY', 'sometoken')
         .expect(200)
-        .then((res) => {
-          const { body } = res;
-          expect(body).to.be.an('array');
-          expect(body).to.have.length(1);
+        .then((res) => expect(res.body).to.deep.equal(responses['200']))
+        .catch((err) => expect(err).to.be.undefined);
+    });
 
-          const user = body[0];
-          expect(user).to.be.an('object');
-          expect(Object.keys(user).length).to.equal(2);
-
-          expect(user.id).to.equal(1);
-          expect(user.name).to.equal('Martijn');
-        })
+    it('should return 200 if query \'api_key\' is provided', async () => {
+      request(app)
+        .get(`${endpoint}?api_key=sometoken`)
+        .expect(200)
+        .then((res) => expect(res.body).to.deep.equal(responses['200']))
         .catch((err) => expect(err).to.be.undefined);
     });
   });

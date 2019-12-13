ecf18838667860fb99d4f9010d91365e7e53a01f
Martijn Vegter <martijn@martijnvegter.com>
2019-12-13 22:27:47 +0100

    feat: added Redis caching

--- server/src/ormconfig.js
+++ server/src/ormconfig.js
@@ -9,7 +9,15 @@ const options = {
   logging: false,
   entities: ["src/entity/**/*.ts"],
   migrations: ["src/migration/**/*.ts"],
-  subscribers: ["src/subscriber/**/*.ts"]
+  subscribers: ["src/subscriber/**/*.ts"],
+  cache: {
+    type: "redis",
+    options: {
+      host: "localhost",
+      port: 6379
+    },
+    duration: 1 * 1000
+  }
 };
 
 if (process.env.CI) {
@@ -35,6 +43,14 @@ if (process.env.MYSQL_PASS) {
 
 if (process.env.MYSQL_NAME) {
   options['database'] = process.env.MYSQL_NAME;
+}
+
+if (process.env.REDIS_HOST) {
+  options['cache']['options']['host'] = process.env.REDIS_HOST;
+}
+
+if (process.env.REDIS_PORT) {
+  options['cache']['options']['port'] = process.env.REDIS_PORT;
 }
 
 module.exports = options;

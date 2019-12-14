c8cf4ade8915494d1872f238bde8d7da18d81ad2
Martijn Vegter <martijn@martijnvegter.com>
2019-12-14 14:08:11 +0100

    test: added unit tests for getToken

---
+++ server/test/helper/index.ts
@@ -0,0 +1,5 @@
+import getToken from './getToken.test';
+
+export default () => {
+  describe('getToken', getToken);
+};
--- server/test/index.test.ts
+++ server/test/index.test.ts
@@ -1,6 +1,8 @@
 import 'mocha';
+import helperTest from './helper';
 import specTest from './spec';
 
 describe('', () => {
+  describe('Helper', helperTest);
   describe('Spec', specTest);
 });

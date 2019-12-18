87f7a5a489e65b6cfe8acf1b086ab5d05c645f0e
Martijn Vegter <martijn@martijnvegter.com>
2019-12-18 22:07:06 +0100

    test: auto generate tests for (Base)Entity classes

--- server/test/entity/index.ts
+++ server/test/entity/index.ts
@@ -1,5 +1,7 @@
+import baseEntity from './baseEntity.test';
 import tokenScope from './tokenScope.test';
 
 export default (): void => {
+    describe('baseEntity', baseEntity);
     describe('tokenScope', tokenScope);
 };

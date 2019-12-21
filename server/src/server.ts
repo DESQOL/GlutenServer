3332ad6c57737556d48391cb6c3e2ab08f59dffd
Martijn Vegter <martijn@martijnvegter.com>
2019-12-21 17:31:23 +0100

    feat: added cluster setup to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,26 @@
+import cluster from 'cluster';
+import os from 'os';
+
 import App from './app';
 
-// tslint:disable-next-line: no-floating-promises
-(async (): Promise<void> => {
-    const app = new App();
-    await app.listen();
-})();
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+
+    // Fork workers.
+    for (let i = 0; i < os.cpus().length; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker: cluster.Worker, _code: number, _signal: string) => {
+        console.log(`worker ${worker.process.pid} died`);
+        cluster.fork();
+    });
+} else {
+    console.log(`Worker ${process.pid} started`);
+
+    // tslint:disable-next-line: no-floating-promises
+    (async (): Promise<void> => {
+        const app = new App();
+        await app.listen();
+    })();
+}

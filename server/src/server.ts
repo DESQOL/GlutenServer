8cc989c14749065fe0a56f766881ceb39179c042
Martijn Vegter <martijn@martijnvegter.com>
2019-12-22 00:34:40 +0100

    refactor: only run cluster in production env

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,26 +1,31 @@
 import cluster from 'cluster';
 import os from 'os';
 
+import { isProduction } from '@helper';
 import App from './app';
 
-if (cluster.isMaster) {
-    console.log(`Master ${process.pid} is running`);
+async function startApp (): Promise<void> {
+    const app = new App();
+    await app.listen();
+}
 
-    // Fork workers.
-    for (let i = 0; i < os.cpus().length; i++) {
-        cluster.fork();
+if (isProduction()) {
+    if (cluster.isMaster) {
+        console.log(`Master ${process.pid} is running`);
+
+        // Fork workers.
+        for (let i = 0; i < os.cpus().length; i++) {
+            cluster.fork();
+        }
+
+        cluster.on('exit', (worker: cluster.Worker, _code: number, _signal: string) => {
+            console.log(`worker ${worker.process.pid} died`);
+            cluster.fork();
+        });
+    } else {
+        console.log(`Worker ${process.pid} started`);
+        (startApp());
     }
-
-    cluster.on('exit', (worker: cluster.Worker, _code: number, _signal: string) => {
-        console.log(`worker ${worker.process.pid} died`);
-        cluster.fork();
-    });
 } else {
-    console.log(`Worker ${process.pid} started`);
-
-    // tslint:disable-next-line: no-floating-promises
-    (async (): Promise<void> => {
-        const app = new App();
-        await app.listen();
-    })();
+    (startApp());
 }

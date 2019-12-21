732b48ff41641fe495a5806d86317b4ac211a50c
Martijn Vegter <martijn@martijnvegter.com>
2019-12-21 17:02:37 +0100

    refactor: added ValidateClassArgs to routes

--- server/src/controller/user.controller.ts
+++ server/src/controller/user.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Route } from '@decorator';
+import { Controller, Route, ValidateClassArgs } from '@decorator';
 import { Token, User } from '@entity';
 import { validate } from 'class-validator';
 import { Request, Response, NextFunction } from 'express';
 import { getRepository } from 'typeorm';
-import { getToken } from '@helper';
+import { getToken, DefaultMessages } from '@helper';
 
 @Controller('/user')
 export class UserController {
@@ -11,15 +11,15 @@ export class UserController {
     private userRepository = getRepository(User);
 
     @Route('post', '/register')
-    public async register (request: Request, response: Response, _next: NextFunction): Promise<object> {
+    @ValidateClassArgs('body', User, { email: 'email', name: 'name', password: 'password' })
+    public async register (request: Request, response: Response, _next: NextFunction): Promise<Response|object> {
         const { email, name, password } = request.body;
 
         let user = await this.userRepository.findOne({ email });
         if (user) {
-            response.status(409).json({
+            return response.status(409).json({
                 message: 'Specified email address is already associated with an account.',
             });
-            return;
         }
 
         user = new User();
@@ -29,10 +29,10 @@ export class UserController {
 
         const errors = await validate(user);
         if (errors.length > 0) {
-            response.status(400).json({
+            return response.status(400).json({
+                message: DefaultMessages[400],
                 errors,
             });
-            return;
         }
 
         user = await this.userRepository.save(user);
@@ -40,23 +40,22 @@ export class UserController {
     }
 
     @Route('post', '/login')
+    @ValidateClassArgs('body', User, { email: 'email', password: 'password' })
     public async login (request: Request, response: Response, _next: NextFunction): Promise<object> {
         const { email, password } = request.body;
 
         const user = await this.userRepository.findOne({ email });
         if (!user) {
-            response.status(403).json({
+            return response.status(403).json({
                 message: 'Incorrect email or password.',
             });
-            return;
         }
 
         const isMatch = await user.validatePassword(password);
         if (!isMatch) {
-            response.status(403).json({
+            return response.status(403).json({
                 message: 'Incorrect email or password.',
             });
-            return;
         }
 
         response.json(user.displayUnit());
